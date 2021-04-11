d90b161295e603e737956696033f7ba0bdad581f
chiknas <nkuknas@gmail.com>
2021-04-11 22:02:24 +0100

    get the last executed trade

--- KrakenBot.py
+++ KrakenBot.py
@@ -1,18 +1,30 @@
 import krakenex
+import pprint
+import json
 
 kraken = krakenex.API()
 kraken.load_key('kraken.key')
 
-minimumBtcVolume = "0.0002"
+# you are not allow to buy less than that
+# current price for minimum around 9GBP
+minimumBtcVolume = 0.0002
 
-print(kraken.query_public("Ticker", {"pair": "XBTGBP"}))
-
-print(kraken.query_private('AddOrder', {
+def placeOrder(type):
+    print(kraken.query_private('AddOrder', {
     "pair": "XBTGBP", 
     "volume": minimumBtcVolume,
-    "type": "buy", 
+    "type": type, 
     "ordertype": "market", 
     "leverage": "none",
     "starttm": "0",
     "expiretm": "+86400"
     }))
+
+def getLastTrade():
+    totalOrders = kraken.query_private('ClosedOrders', {"ofs": "0", "closetime": "close"})["result"]["count"]
+    trades = kraken.query_private('ClosedOrders', {"ofs": totalOrders - 1, "closetime": "close"})["result"]["closed"]
+    return trades[list(trades)[0]]
+
+    
+    
+pprint.pprint(getLastTrade())
