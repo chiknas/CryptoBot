17eadcc1051fa26bb9b9b9c309e9706e0b48effa
chiknas <nkuknas@gmail.com>
2021-04-11 23:19:35 +0100

    calculate the current profit margin

--- KrakenBot.py
+++ KrakenBot.py
@@ -1,6 +1,6 @@
 import krakenex
 import pprint
-import json
+import time
 
 kraken = krakenex.API()
 kraken.load_key('kraken.key')
@@ -8,10 +8,11 @@ kraken.load_key('kraken.key')
 # you are not allow to buy less than that
 # current price for minimum around 9GBP
 minimumBtcVolume = 0.0002
+cryptoCode = "XBTGBP"
 
 def placeOrder(type):
     print(kraken.query_private('AddOrder', {
-    "pair": "XBTGBP", 
+    "pair": cryptoCode, 
     "volume": minimumBtcVolume,
     "type": type, 
     "ordertype": "market", 
@@ -21,10 +22,26 @@ def placeOrder(type):
     }))
 
 def getLastTrade():
-    totalOrders = kraken.query_private('ClosedOrders', {"ofs": "0", "closetime": "close"})["result"]["count"]
-    trades = kraken.query_private('ClosedOrders', {"ofs": totalOrders - 1, "closetime": "close"})["result"]["closed"]
+    trades = kraken.query_private('TradesHistory', {"ofs": "0"})["result"]["trades"]
     return trades[list(trades)[0]]
 
-    
-    
-pprint.pprint(getLastTrade())
+def getCurrentMargin():
+    """
+    Returns the percentage of increase/decrease in price since the last order.
+    If the last order was sell so there is no current coin in our possesion then 
+    it returns 0.
+    ex. if we bought at 10 and now price is 0 this will return -10
+    """
+    lastTrade = getLastTrade()
+    isInvested = lastTrade["type"] == "buy"
+    if isInvested:
+        tickerResult = kraken.query_public("Ticker", {"pair": cryptoCode})["result"]
+        currentBtcPrice = tickerResult[list(tickerResult)[0]]["c"][0]
+        lastTradePrice = lastTrade["price"]
+        return  ((float(currentBtcPrice) - float(lastTradePrice)) / float(lastTradePrice)) * 100
+    else:
+        return 0
+
+while True:
+    pprint.pprint(getCurrentMargin())
+    time.sleep(10)
