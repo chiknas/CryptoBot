1d370214eb41a6adb33d00f820ed92ea68750e69
chiknas <nkuknas@gmail.com>
2021-05-08 22:03:44 +0100

    utilise obv calculator to get obv indicator values for a timeframe of the pair

--- KrakenBot.py
+++ KrakenBot.py
@@ -1,14 +1,25 @@
 import krakenex
 import pprint
 import time
+from ObvCalculator import ObvCalculator
 
 kraken = krakenex.API()
 kraken.load_key('kraken.key')
+obv = ObvCalculator()
+
+# KRAKEN_DATA_LABELS
+#   a: "Ask",
+#   b: "Bid",
+#   c: "Last Trade Closed",
+#   v: "Volume",
+#   l: "Low",
+#   h: "High",
+#   o: "Today Opening Price"
 
 # you are not allow to buy less than that
 # current price for minimum around 9GBP
 minimumBtcVolume = 0.0002
-cryptoCode = "XBTGBP"
+cryptoCode = "BTCGBP"
 
 def placeOrder(type):
     print(kraken.query_private('AddOrder', {
@@ -42,6 +53,39 @@ def getCurrentMargin():
     else:
         return 0
 
+tickers = []
+
+def getBitcoinTicker():
+    return kraken.query_public("Ticker", {"pair": cryptoCode})["result"]["XXBTZGBP"]
+
+def serializeTickers():
+    openValue = []
+    highValue = []
+    lowValue = []
+    closeValue = []
+    volumeValue = []
+
+    for ticker in tickers:
+        openValue.append(float(ticker["o"]))
+        highValue.append(float(ticker["h"][0]))
+        lowValue.append(float(ticker["l"][1]))
+        closeValue.append(float(ticker["c"][0]))
+        volumeValue.append(float(ticker["v"][0]))
+
+    return {
+        "open": openValue,
+        "high": highValue,
+        "low": lowValue,
+        "close": closeValue,
+        "volume": volumeValue
+    }
+
+
 while True:
-    pprint.pprint(getCurrentMargin())
+    currentTicker = getBitcoinTicker()
+    tickers.append(currentTicker)
+    if len(tickers) > 10:
+        tickers.pop(0)
+
+    pprint.pprint(obv.currentObv(serializeTickers()))
     time.sleep(10)
